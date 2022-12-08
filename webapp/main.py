a1b93f31125a933a9163d6f2574232591eb80240
mfritscher555 <mfritscher555@yahoo.com>
2022-12-08 15:26:24 -0600

    fixing input box

--- webapp/main.py
+++ webapp/main.py
@@ -1,8 +1,8 @@
 
 
 from flask import Flask
-from flask import render_template, request
-from logging import FileHandler, WARNING
+from flask import render_template, request, redirect
+# from logging import FileHandler, WARNING
 import math
 
 app = Flask(__name__, template_folder='Templates')
@@ -23,34 +23,31 @@ def index():
     return render_template("index.html", area=area, circumference=circumference, radius=radius)
 
 
-@app.route('/', methods=['Post'])
+@app.route('/', methods=['GET','POST'])
 def indexPost():
-    radius = request.form['radius']
-    # If the input is good:
-    try:
-        calculatedArea = area(float(radius))
-        calculatedCircum = circumference(float(radius))
-        return render_template("index.html", area=calculatedArea, circumference=calculatedCircum, radius=radius)
-    except ValueError:
+    if request.method == "POST":
+        radius = request.form['radius']
+        # If the input is good:
         try:
-            # If the input contains commas
-            radius = radius.replace(",", "")
             calculatedArea = area(float(radius))
             calculatedCircum = circumference(float(radius))
             return render_template("index.html", area=calculatedArea, circumference=calculatedCircum, radius=radius)
         except ValueError:
-            return render_template("index.html", area="Invalid input", circumference="Invalid input", radius=radius)
-
-
-
+            try:
+                # If the input contains commas (not necessary when input changed to number)
+                radius = radius.replace(",", "")
+                calculatedArea = area(float(radius))
+                calculatedCircum = circumference(float(radius))
+                return render_template("index.html", area=calculatedArea, circumference=calculatedCircum, radius=radius)
+            except ValueError: # when the input is empty set these equal to empty as well
+                return render_template("index.html", area="", circumference="", radius=radius)
+    elif request.method == "GET":
+        return redirect('/')
 
 
 def area(radius):
     value = pi * radius ** 2
     return value
-
-
-
 
 
 
